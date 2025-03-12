edc6de6e5e33ac38a4a219ba85a98130dcad978b
fredrik <freedrik92@hotmail.com>
2025-03-12 12:56:53 +0100

    Add project files.

---
+++ TestAppMVC/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestAppMVC.Controllers;
+
+public class AuthController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+    public IActionResult CreateAccount()
+    {
+        return View();
+    }
+}
---
+++ TestAppMVC/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿document.querySelector("#dark-mode-toggle").addEventListener("click", function () {
+    const htmlElement = document.documentElement;
+
+    if (htmlElement.getAttribute("data-theme") === "light") {
+        htmlElement.setAttribute("data-theme", "dark");
+    } else {
+        htmlElement.setAttribute("data-theme", "light");
+    }
+});
+// document.querySelector connects to id dark-mode-toggle
+// addEventLister listener for a click-event.
+//gets the html element to be able to modify the attribut, it saves it in a const called htmlElement
+//if the html has a attribute called data-theme and it is set to dark, then it will set data-theme to light
+//if its not dark it will set it to dark:
